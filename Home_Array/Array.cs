a68c61d0259dcb0a596b54a2b9b2bd3c4c9acdf9
Abror <abrorhalimov660@gmail.com>
2025-02-22 20:21:44 +0500

    Add project files.

---
+++ Home_Array/Array.cs
@@ -0,0 +1,160 @@
+﻿namespace Home_Array
+{
+    public class Array
+    {
+        public void ShowOnTheWindowArray(string[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public int SumOfArray(int[][] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sum += array[i][j];
+                }
+            }
+            return sum;
+        }
+
+        public void FindMax(int[][] array)
+        {
+            int max = array[0][0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] > max)
+                    {
+                        max = array[i][j];
+
+                    }
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+        public void FindMin(int[][] array)
+        {
+            int min = array[0][0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] < min)
+                    {
+                        min = array[i][j];
+
+                    }
+                }
+            }
+            Console.WriteLine(min);
+        }
+
+        public void FindSumEachOfThem(int[][] array)
+        {
+
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    sum += array[i][j];
+                }
+
+                Console.WriteLine(sum);
+
+            }
+            
+
+
+        }
+
+        public void FindEven(int[][] array)
+        {
+            
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] % 2 == 0)
+                    {
+                        Console.WriteLine(array[i][j]);
+                    }
+                }
+            }
+
+
+        }
+
+        public void FindOdd(int[][] array)
+        {
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] % 2 == 1)
+                    {
+                        Console.WriteLine(array[i][j]);
+                    }
+                }
+            }
+
+
+        }
+
+        public void FindLengh(int[][] array)
+        {
+            for(int i = 0;i < array.Length; i++)
+            {
+                    Console.WriteLine($" {i+1}: {array[i].Length}");
+            }
+        }
+
+        public bool FindNUmber(int[][] array,int num)
+        {
+            for(int i = 0; i < array.Length ; i++)
+            {
+                for(int j = 0; j < array[i].Length ; j++)
+                {
+                    if(array[i][j] == num)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void ReverseNumber(int[] num)
+        {
+            
+            for(int i = 0; i < num.Length ; i++)
+            {
+                
+                Console.WriteLine(num[i]);
+ 
+            }
+
+          
+        }
+    }
+}
---
+++ Home_Array/Program.cs
@@ -0,0 +1,211 @@
+﻿namespace Home_Array;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var GetClass = new Array();
+
+        /////////////////////////////////////// 1)
+
+        //string[][] task1 = new string[3][]
+        //{
+        //    ["car","home","table"],
+        //    ["phone","mouse","bottle"],
+        //    ["handphone","keeyboard","hat"],
+        //};
+
+        //GetClass.ShowOnTheWindowArray(task1);
+
+        /////////////////////////////////////////////// 2)
+
+        //int[][] num = new int[2][]
+        //{
+        //   [12,32,5,76,],
+        //   [4,3,9,65,]
+        //};
+
+
+        //Console.WriteLine($" Sum: {GetClass.SumOfArray(num)}"); 
+
+        ///////////////////////////////////////////////////////////////////// 3)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [912,32,5,761,],
+        //   [4,3,9,65,],
+        //   [4,33,91,65,]
+        //};
+
+        //GetClass.FindMax(num);
+
+        //////////////////////////////////////////// 4)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [912,32,5,761,],
+        //   [4,3,9,65,],
+        //   [4,33,91,65,]
+        //};
+
+        //GetClass.FindMin(num);
+
+        /////////////////////////////////////////// 5)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [1,2,5,1,],
+        //   [4,3,9,5,],
+        //   [4,3,9,6,]
+        //};
+
+        //GetClass.FindSumEachOfThem(num);
+
+        /////////////////////////////////////////// 6)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [11,52,51,11,],
+        //   [4,73,93,53,],
+        //   [4,31,9,65,]
+        //};
+
+        //GetClass.FindEven(num);
+
+        ////////////////////////////////////////// 7)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [1,2,5,1,],
+        //   [4,3,9,5,],
+        //   [4,3,9,6,]
+        //};
+
+        //GetClass.FindOdd(num);
+
+        //////////////////////////////////////////// 8)
+
+        //int[][] num = new int[3][]
+        //{
+        //   [11,5,61,17,],
+        //   [4,83,49,93,],
+        //   [4,32,59,65,]
+        //};
+
+        //GetClass.FindLengh(num);
+
+        ////////////////////////////////////////////////// 9)
+
+        //Console.WriteLine("Enter number");
+        //int num = int.Parse(Console.ReadLine());
+
+        //int[][] digits = new int[][]
+        //{
+        //    [12,32,54,65],
+        //    [87,5,74,35],
+        //    [81,22,4,65]
+        //};
+
+        //Console.WriteLine(GetClass.FindNUmber(digits, num)); 
+
+        //////////////////////////////////////// 10)
+
+        int[] num = { 41, 21, 33, 4, 31, 43};
+        int left = 0;
+        int right = num.Length-1;
+
+        while (left < right)
+        {
+            int temp = num[left];
+            num[left] = num[right];
+            num[right] = temp;
+
+            left++;
+            right--;
+        }
+        
+        foreach (int i in num)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public void Examples()
+    {
+        //int[] numbers = { 11, 2, 1, 32, 12, 93, 54 };
+        //int[] res = new int[6];
+
+        //var num = numbers.OrderBy(x => x).ToList();
+        //foreach (int x in num)
+        //{
+        //    Console.Write($"{x},");
+        //}
+        //Console.WriteLine();
+        //var number = numbers.OrderByDescending(x => x).ToList();
+        //foreach (int x in number)
+        //{
+        //    Console.Write($"{x},");
+        //}
+
+        //////////////////////////////////////////////////
+
+        //string[] num = {"Abror","Ulmas","Axror"};
+
+
+        //for (int i = 0; i < num.Length; i++){
+
+        //    Console.WriteLine(num[i] + $" {i+1}00");
+
+        //}
+
+        ////////////////////////////////////////////////////
+        ///
+
+        //string[][] name = new string[3][]
+        //{
+        //    ["Abror","Axror","Ulmas"],
+
+        //    ["Ulug","Ziyod","Maha"],
+
+        //    ["Ali","Bektemir","Abdurashid"],
+        //};
+        //string[][] shops = new string[3][]
+        //{
+        //    ["shop 1","shop 2","shop 3"],
+
+        //    ["shop 4","shop 5","shop 6"],
+
+        //    ["shop 7","shop 8","shop 9"],
+        //};
+
+
+
+        //for (int i = 0; i < shops.Length; i++)
+        //{
+
+
+        //    for (int j = 0; j < shops[i].Length; j++)
+        //    {
+        //        Console.Write($"{shops[i][j]} - {name[i][j]},");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        ////////////////////////////////////////////////////////////////////
+
+        //for (int i = 0; i < num.Length; i++)
+        //{
+        //    Array.Sort(num);
+        //    Console.Write($"{num[i]},");
+        //}
+
+        //Console.WriteLine();
+
+        //for (int i = 0; i < num.Length; i++)
+        //{
+        //    Array.Reverse(num);
+        //    Console.Write($"{num[i]},");
+        //}
+    }
+
+}
